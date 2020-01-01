fce5dc9074932cd1347b302071f2427afe9f3fa7
zhaohongjie <zhaohongjie@actionsky.com>
2020-01-01 18:38:52 +0800

    binary search added

--- mymain.py
+++ mymain.py
@@ -2,7 +2,7 @@
 # @Time    : 2020/1/1 PM4:13
 # @Author  : zhaohongjie@jianhan513.com
 
-from binary_search import binary_search
+from algorithms.binary_search import binary_search
 
 if __name__ == "__main__":
     print(binary_search([1,2,3,4,5,6,7,8,9,10],7))
